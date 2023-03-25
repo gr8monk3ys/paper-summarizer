719a9671c8f07752dc841e6c66b1cdf042862b0e
gr8monk3ys <lorenzosca7@protonmail.ch>
2023-03-24 18:12:03 -0700

    Included setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="my_package",
+    version="1.0",
+    author="Jane Doe",
+    author_email="jane.doe@example.com",
+    description="A simple Python package",
+    install_requires=["requests", "bs4", "sumy", "nltk", "flask"],
+)
